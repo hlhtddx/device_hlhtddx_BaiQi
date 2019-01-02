4560f93f858978c41061bbc2fa5dbe4cd7a03c25
Frank Zhang <frankzhang123@gmail.com>
2019-01-02 23:27:42 +0800

    Make BaiQi inherits from both Flounder and Car Product
    Change-Id: Ic1c5c066b2110ba52546bbc88b75dfdf088d6bad

---
+++ BaiQi.mk
@@ -0,0 +1,60 @@
+# Lineage specific changes
+
+# Inherit car device configuration
+$(call inherit-product, vendor/hlhtddx/car/common/car.mk)
+
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2048
+TARGET_SCREEN_WIDTH := 1536
+
+# Inherit device configuration
+$(call inherit-product, device/htc/flounder/aosp_flounder.mk)
+
+$(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
+
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/htc/flounder
+TARGET_KERNEL_CONFIG := lineage_flounder_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
+# Extra Packages
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N9F27M 4333998 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/volantis/flounder:7.1.1/N9F27M/4333998:user/release-keys
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2017-10-05
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := BaiQi
+PRODUCT_DEVICE := flounder
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 9
+
+# Add car related sepolicy.
+BOARD_SEPOLICY_DIRS += \
+    vendor/hlhtddx/car/common/sepolicy
+
+# Set device specified manifest.xml
+PRODUCT_COPY_FILES += \
+    device/hlhtddx/BaiQi/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
+
+PRODUCT_COPY_FILES += \
+    device/htc/flounder/gps/bcm/gps.bcm47521.conf:system/etc/gps.bcm47521.conf \
+    device/htc/flounder/gps/bcm/gpsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpsconfig.xml
+
+DEVICE_PACKAGE_OVERLAYS := \
+    $(LOCAL_PATH)/wifi_only_overlay \
+    $(LOCAL_PATH)/overlay
+
+# Set SELinux to Permissive mode for vehicle hal network accessing
+# TODO this will be removed while sepolicy is ready
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
